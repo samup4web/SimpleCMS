f35e55934a4ea936e7b61e7bdb7d3bde6e5daa8a
Samuel Idowu <samup4web@gmail.com>
2012-06-16 19:23:54 +0900

    public view and site root route

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -12,4 +12,10 @@ class DemoController < ApplicationController
   def escape_output
 
   end
+
+  def make_error
+    #render(:text => "test #syntax error
+    #render(:text => @somethin.upcase) #undefined method
+    #render(:text => "1" + 1) #can't convert'
+  end
 end
